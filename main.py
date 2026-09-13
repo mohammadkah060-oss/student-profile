f764c765338f48e249f29f813ea7f1f1680de3f7
Mohammad alabadi <mohammadkah060@gmail.com>
2026-09-13 13:02:59 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,12 @@
+# Student Profile
+# This program displays information about a student.
+
+name = "Mohammad"
+major = "Software Engineering"
+university = "Al-Zaytoonah University of Jordan"
+
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
