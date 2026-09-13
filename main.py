038712c139037ea9b620fa532da44c6e68a8c529
Mohammad alabadi <mohammadkah060@gmail.com>
2026-09-13 14:55:19 +0300

    Add more student information

--- main.py
+++ main.py
@@ -4,9 +4,10 @@
 name = "Mohammad"
 major = "Software Engineering"
 university = "Al-Zaytoonah University of Jordan"
-
+graduation_year = 2027
 print("Student Profile")
 print("----------------")
 print("Name:", name)
 print("Major:", major)
 print("University:", university)
+print("Graduation Year:", graduation_year)
